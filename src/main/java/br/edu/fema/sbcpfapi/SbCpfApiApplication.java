eef5e8115f6b62de7c8bb2667aed3daf8bfa72b5
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-06-14 09:48:54 -0300

    Initial commit

---
+++ src/main/java/br/edu/fema/sbcpfapi/SbCpfApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fema.sbcpfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCpfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCpfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fema/sbcpfapi/SbCpfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fema.sbcpfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCpfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
