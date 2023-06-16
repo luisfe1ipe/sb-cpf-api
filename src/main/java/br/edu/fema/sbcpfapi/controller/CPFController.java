7d91b17abef2f706bc2f5a644e72c26d3158c5fb
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-06-16 20:52:48 -0300

    validação cpf part.1

--- src/main/java/br/edu/fema/sbcpfapi/controller/CPFController.java
+++ src/main/java/br/edu/fema/sbcpfapi/controller/CPFController.java
@@ -1,17 +1,19 @@
 package br.edu.fema.sbcpfapi.controller;
 
 import br.edu.fema.sbcpfapi.domain.cpf.service.CPFService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cpf")
 public class CPFController {
 
+    @Autowired
     private CPFService cpfService;
 
     @GetMapping("/{cpf}")
-    public String cpf(@PathVariable("cpf") String cpf) {
-        final String message = cpfService.validar(cpf);
+    public Boolean cpf(@PathVariable("cpf") String cpf) {
+        final Boolean message = cpfService.validate(cpf);
         return message;
     }
 
--- src/main/java/br/edu/fema/sbcpfapi/domain/cpf/service/CPFService.java
+++ src/main/java/br/edu/fema/sbcpfapi/domain/cpf/service/CPFService.java
@@ -7,11 +7,46 @@ import java.util.List;
 @Service
 public class CPFService {
 
-    public boolean validate(String cpf){
-        return false;
+    public boolean validate(String cpf) {
+        int somaUm = 0;
+        int somaDois = 0;
+        int numeroDez = Integer.parseInt(cpf.substring(9, 10));
+        int numeroOnze = Integer.parseInt(cpf.substring(10, 11));
+        System.out.println(numeroDez);
+        System.out.println(numeroOnze);
+
+        //Soma 10° digito
+        for (int i = 0; i < 9; i++) {
+            int numero = Integer.parseInt(cpf.substring(i, i + 1));
+            somaUm = somaUm + numero * (10 - i);
+        }
+
+        System.out.println(somaUm);
+        double restoUm = (somaUm * 10.0) % 11;
+        if (restoUm == 10) {
+            restoUm = 0;
+        }
+        System.out.println(restoUm);
+
+        //Soma 11° digito
+        for (int i = 0; i < 10; i++) {
+            int numero = Integer.parseInt(cpf.substring(i, i + 1));
+            somaDois = somaDois + numero * (11 - i);
+        }
+
+        double restoDois = (somaDois * 10.0) % 11;
+        if (restoDois == 10) {
+            restoDois = 0;
+        }
+
+        if (restoUm == numeroDez && restoDois == numeroOnze) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public String (String cpf){
-        return cpf;
+    public static void main(String[] args) {
+        new CPFService().validate("42225443882");
     }
 }
